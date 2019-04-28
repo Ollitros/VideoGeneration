c19f16e0887823c0860470ebc8a0f5bb2fa377aa
Ollitros <Ollitros@gmail.com>
2019-04-28 13:29:10 +0300

    train_model.py improvements

---
+++ train_model.py
@@ -0,0 +1,86 @@
+import numpy as np
+import cv2 as cv
+import time
+from network.model import Gan
+
+
+def train_model(input_shape, train_x, epochs, batch_size):
+    model = Gan(input_shape=input_shape, image_shape=(64, 64, 6))
+    model.build_train_functions()
+
+    errG_sum = errD_sum = 0
+    display_iters = 1
+
+    t0 = time.time()
+    model.load_weights()
+
+    iters = np.asarray(train_x).shape[0] // batch_size
+    if np.asarray(train_x).shape[0] - iters * batch_size == 0:
+        train_x.append(train_x[-1])
+
+    train_x = np.asarray(train_x)
+    for epoch in range(epochs):
+
+        # Train generator predict first frame form noise
+        # Train discriminator
+        batch_y = []
+        for f in range(batch_size):
+            batch_y.append(train_x[0])
+        batch_noise = np.random.normal(0, 1, (batch_size, input_shape[0], input_shape[1], input_shape[2]))
+        fake = model.generator.predict(batch_noise)
+        errD = model.train_discriminator(X=np.float32(fake)[:, :, :, :3], Y=batch_y)
+        errD_sum += errD[0]
+        # Train generator
+        errG = model.train_generator(X=batch_noise, Y=batch_y)
+        errG_sum += errG[0]
+
+        # Train others frames
+        # Train discriminator
+        step = 0
+        for iter in range(iters):
+            fake = model.generator.predict(train_x[step: (step + batch_size)])
+            errD = model.train_discriminator(X=np.float32(fake)[:, :, :, :3],  Y=train_x[(step+1): (step+1 + batch_size)])
+            step = step + batch_size
+        errD_sum += errD[0]
+
+        # Train generator
+        step = 0
+        for iter in range(iters):
+            errG = model.train_generator(X=train_x[step:(step + batch_size)], Y=train_x[(step+1):(step+1 + batch_size)])
+            step = step + batch_size
+        errG_sum += errG[0]
+
+        # Visualization
+        if epoch % display_iters == 0:
+            print("----------")
+            print('[iter %d] Loss_D: %f Loss_G: %f  time: %f' % (epoch, errD_sum / display_iters,
+                                                                 errG_sum / display_iters, time.time() - t0))
+            print("----------")
+            display_iters = display_iters + 1
+
+        if epoch % 1 == 0:
+            # Makes predictions after each epoch and save into temp folder.
+            prediction = model.generator.predict(train_x[0:2])
+            cv.imwrite('data/models/temp/image{epoch}.jpg'.format(epoch=epoch + 1900), prediction[0] * 255)
+
+    model.save_weights()
+
+
+def main():
+
+    # Load the dataset
+    X = np.load('data/source/X.npy')
+    X = X.astype('float32')
+    X /= 255
+
+    epochs = 100
+    batch_size = 5
+    input_shape = (64, 64, 3)
+
+    train_model(input_shape=input_shape, train_x=X, epochs=epochs, batch_size=batch_size)
+
+
+if __name__ == '__main__':
+    main()
+
+
